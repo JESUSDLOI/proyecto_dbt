7da13f1cea51ae7e4dc4d0e12f032ba458e328ae
JESUSDLOI <jesus.delaoliva.ig@gmail.com>
2024-12-03 23:49:29 +0100

    Solucion problemas

--- proyecto_finanzas/macros/fecha_inferiar_actual.sql
+++ proyecto_finanzas/macros/fecha_inferiar_actual.sql
@@ -1,5 +1,5 @@
 {% macro test_fecha_inferior_actual(model, column_name) %}
   SELECT *
   FROM {{ model }}
-  WHERE {{ column_name }} >= CURRENT_TIMESTAMP
+  WHERE {{ column_name }} > CONVERT_TIMEZONE('UTC', CURRENT_TIMESTAMP)
 {% endmacro %}
--- proyecto_finanzas/models/staging/api_alpha/bases/base_api_alpha__DLT_LOADS.sql
+++ proyecto_finanzas/models/staging/api_alpha/bases/base_api_alpha__DLT_LOADS.sql
@@ -17,7 +17,7 @@ WITH source AS (
     CAST(LOAD_ID AS VARCHAR(167778)) AS id_carga_dlt, /* Identificador único de la carga */
     CAST(SCHEMA_NAME AS VARCHAR(167778)) AS nombre_esquema, /* Nombre del esquema */
     CAST(STATUS AS VARCHAR(167778)) AS estado, /* Estado de la carga */
-    CONVERT_TIMEZONE('UTC', CAST(INSERTED_AT AS TIMESTAMP)) AS fecha_insercion, /* Fecha y hora en que se insertó la carga en UTC */
+    CONVERT_TIMEZONE('UTC', INSERTED_AT) AS fecha_insercion, /* Fecha y hora en que se insertó la carga en UTC */
     CAST(SCHEMA_VERSION_HASH AS VARCHAR(255)) AS hash_version_esquema /* Hash de la versión del esquema */
   FROM source
 )
